cb2b1931f361d49d8eabfceecfd14c01f99335d9
Mark Moore <aiaeaoo@gmail.com>
2013-05-14 18:57:26 -0400

    template details

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ if ( !defined('ABSPATH')) exit;
  *
  *
  * @file           index.php
- * @package        Responsive 
- * @author         Emil Uzelac 
- * @copyright      2003 - 2013 ThemeID
+ * @package        Custom FLyBoard 
+ * @author         Mark Moore
+ * @copyright      2013 fbmh.co
  * @license        license.txt
  * @version        Release: 1.0
- * @filesource     wp-content/themes/responsive/index.php
+ * @filesource     wp-content/themes/CustomFlyB/index.php
  * @link           http://codex.wordpress.org/Theme_Development#Index_.28index.php.29
  * @since          available since Release 1.0
  */
